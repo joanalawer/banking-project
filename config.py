cdd16e576b8caa91883bb677cfbc9bcd34f26a77
joanalawer <lawerjoana477@gmail.com>
2023-10-18 09:00:28 +0000

    Update class Config

--- config.py
+++ config.py
@@ -4,9 +4,12 @@ basedir = os.path.abspath(os.path.dirname(__file__))    # Returns the banking-pr
 class Config:           
     SECRET_KEY = os.environ.get('SECRET_KEY', default= os.environ.get('SECRET_KEY'))
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    FLASKY_MAIL_SUBJECT_PREFIX = '[PeriBank]'
+    FLASKY_MAIL_SENDER = 'PeriBank Admin <jlawer90@gmail.com>'
+    FLASKY_ADMIN = os.environ.get('PERIBANK_ADMIN')
 
     @staticmethod
-    def init_app(peripheral):
+    def init_app(app):
         pass
 
 class DevelopmentConfig(Config):
