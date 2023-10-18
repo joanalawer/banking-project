152b4f76528644c3cd1f6afc8e2fd10a2e64c0ca
joanalawer <lawerjoana477@gmail.com>
2023-10-18 19:01:36 +0000

    Update register to auth/

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,9 +1,13 @@
 from flask_wtf import Form
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Email, Length
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, ValidationError
+from wtforms.validators import DataRequired, Email, Length, EqualTo, Regexp
+from models import User
 
 class CustomerLogin(Form):
     email = StringField('Username', validators=[DataRequired(), Length(1, 64), Email()])
     password = PasswordField('Password', validators=[DataRequired(), Length(min=8, message='Password must be at least 8 characters long')])
     remember_me = BooleanField('Remember me')
     submit = SubmitField('Login')
+
+class RegistrationForm(From):
+    
