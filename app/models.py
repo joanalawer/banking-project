21440899ca63f7534c0dc0b48b99145d18ce869f
joanalawer <lawerjoana477@gmail.com>
2023-10-18 10:51:05 +0000

    Modify user model

--- app/models.py
+++ app/models.py
@@ -3,33 +3,31 @@ from flask_sqlalchemy import SQLAlchemy
 from __init__ import login_manager, db
 import datetime
 from sqlalchemy import DateTime
+from werkzeug.security import generate_password_hash, check_password_hash
 
-class Users(UserMixin, db.Model):
+class Users(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key = True, autoincrement=True)
-    username = db.Column(db.String(128))
-    password = db.Column(db.String(128))
-    acc_number = db.Column(db.Integer, unique=True, index=True)
-    is_active = db.Column(db.Boolean)
-    last_login = db.Column(DateTime, default=datetime.datetime.now )
-
-    def __init__(self, **kwargs):
-        for property, value in kwargs.items():
-            if hasattr(value, '__iter__') and not isinstance(value, str):
-                value = value[0]
-            setattr(self, property, value)
-    def get_id(self):
-        return (self.id)
-
-    def is_authenticated(self):
-        return True
-
-    def is_active(self):
-        return True
-
-    def is_anonymous(self):
-        return False
-
+    username = db.Column(db.String(128), unique=True, index=True)
+    password_hash = db.Column(db.String(128))
+    # acc_number = db.Column(db.Integer, unique=True, index=True)
+    # is_active = db.Column(db.Boolean)
+    # last_login = db.Column(DateTime, default=datetime.datetime.now )
+    
+    @property
+	def password(self):
+	    raise AttributeError('password is not a readable attribute')
+	
+	@password.setter
+	def password(self, password):
+		self.password_hash = generate_password_hash(password)
+	
+	def verify_password(self, password):
+		return check_password_hash(self.password_hash, password)
+	
+	def __repr__(self):
+		return '<User %r>' % self.username
+        
 class Customer(db.Model):
     __tablename__ = 'customers'
     id = db.Column(db.Integer, primary_key = True, autoincrement=True)
