470a9db3faf764905351d49a2a192205daba5cf4
joanalawer <lawerjoana477@gmail.com>
2023-10-18 10:00:55 +0000

    Update manage with test launcher command

--- manage.py
+++ manage.py
@@ -11,5 +11,13 @@ def make_shell_context():
     return dict(app=app, db=db, User=User, Role=Role)
 manager.add_command('db', MigrateCommand)
 
+# Unit test launcher command
+# @manager.command
+# def test():
+#     """Run the unit tests."""
+#     import unittest
+#     tests = unittest.TestLoader().discover('tests')
+#     unittest.TextTestRunner(verbosity=2).run(tests)
+
 if __name__ == '__main__':
     manager.run()
