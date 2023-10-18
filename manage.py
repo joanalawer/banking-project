f5eeb2385d76679c831b403196afdc21c8e4cbfb
joanalawer <lawerjoana477@gmail.com>
2023-10-18 09:55:57 +0000

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,14 +1,15 @@
 import os
 from app import create_app, db
 from app.models import User, Role
-from flask.ext.script import Manager, Shell
-from flask.ext.migrate import Migrate, MigrateCommand
-app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+from flask_migrate import Migrate, MigrateCommand
+
+app = create_app(os.getenv('PERIBANK_CONFIG') or 'default')
 manager = Manager(app)
 migrate = Migrate(app, db)
+
 def make_shell_context():
-return dict(app=app, db=db, User=User, Role=Role)
-manager.add_command("shell", Shell(make_context=make_shell_context))
+    return dict(app=app, db=db, User=User, Role=Role)
 manager.add_command('db', MigrateCommand)
+
 if __name__ == '__main__':
-manager.run()
+    manager.run()
