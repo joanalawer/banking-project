ca99d28f5b0f117f16d150ec8d32f6834806a529
joanalawer <lawerjoana477@gmail.com>
2023-10-18 11:10:12 +0000

    Register auth_blueprint

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,10 +1,5 @@
 from flask import render_template
-from main import blueprint
 from auth import auth
-
-@blueprint.route('/')
-def index():
-    return render_template('index.html')
 
 @auth.route('/login')
 def login():
