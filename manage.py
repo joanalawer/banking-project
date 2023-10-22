199285178ff558b881aa63b037b2e244d2195d7d
joanalawer <lawerjoana477@gmail.com>
2023-10-22 20:54:06 +0000

    Remove role object

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask
 from app import create_app, db
-from app.models import User, Role
+from app.models import User
 from flask_migrate import Migrate
 
 app = create_app('default')
@@ -9,7 +9,7 @@ app = Flask(__name__)
 migrate = Migrate(app, db)
 
 def make_shell_context():
-    return dict(app=app, db=db, User=User, Role=Role)
+    return dict(app=app, db=db, User=User)
 
 # Unit test launcher command
 # @manager.command
